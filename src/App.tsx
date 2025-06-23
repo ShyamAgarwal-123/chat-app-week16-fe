89b96e91830a254c598b0c48f3a3565951d653a4
Shyam Agarwal <shyamagarwal3800@gmail.com>
2025-06-23 22:02:37 +0530

    render url added

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,7 @@ function App() {
   };
 
   useEffect(() => {
-    const ws = new WebSocket("ws://localhost:8080");
+    const ws = new WebSocket("https://chat-app-week16-fe-cnxp.vercel.app");
     setSocket(ws);
 
     ws.onmessage = (ev) => {
